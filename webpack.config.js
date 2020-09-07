20512b0faf4f80fd0e657e230e5351a08e954088
Filipe Merker <fmerk@softserveinc.com>
2020-09-08 01:20:48 +0200

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     path: __dirname + "/dist",
     filename: "[name].js",
     chunkFilename: "[name].[id].js",
-    publicPath: "https://filipemerker.github.io/module-federation-login",
+    publicPath: "https://filipemerker.github.io/module-federation-login/",
   },
   module: {
     rules: [
